84af9b48d3dd2e63291d53511cee78a36351f9fe
Justin Liu <justinliu@ark7.com>
2024-05-14 18:03:50 -0400

    add TestPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 import MainPage from './pages/MainPage';
+import TestPage from './pages/TestPage';
 
 export default function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<MainPage/>} />
+        <Route path="/test" element={<TestPage/>} />
 
       </Routes>
     </Router>
