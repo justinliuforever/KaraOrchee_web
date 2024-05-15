a3b1cfce8b7cbd15595cbbd03da6ddcadc177f74
Justin Liu <justinliu@ark7.com>
2024-05-14 21:49:55 -0400

    edit

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -51,7 +51,7 @@ export default function Features() {
   return (
     <>
       {/* Primary Features Section */}
-      <div className="mx-auto mt-32 max-w-7xl sm:mt-56 sm:px-6 lg:px-8">
+      {/* <div className="mx-auto mt-32 max-w-7xl sm:mt-56 sm:px-6 lg:px-8">
         <div className="relative isolate overflow-hidden bg-gray-900 px-6 py-20 sm:rounded-3xl sm:px-10 sm:py-24 lg:py-24 xl:px-24">
           <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-center lg:gap-y-0">
             <div className="lg:row-start-2 lg:max-w-md">
@@ -99,7 +99,7 @@ export default function Features() {
             />
           </div>
         </div>
-      </div>
+      </div> */}
 
       {/* Secondary Features Section */}
       <div className="mx-auto mt-32 max-w-7xl px-6 sm:mt-56 lg:px-8">
