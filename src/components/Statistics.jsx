650fba8fb79b0b09e2721a9a5e91f2c09fb2b64c
Justin Liu <justinliu@ark7.com>
2024-05-21 10:52:15 -0400

    change and finish the statistics feature

--- src/components/Statistics.jsx
+++ src/components/Statistics.jsx
@@ -1,31 +1,24 @@
-/* 交流QQ群:七五四5七三七78 */
 const stats = [
   { id: 1, name: 'SOM Users who engage in Classical music willing to pay $100/year', value: '800,000' },
   { id: 2, name: 'Total Market Opportunity', value: '$200 Million' },
-  { id: 3, name: 'Al Integrated Online Community', value: 'Revolutionizing Lifestyle' },
+  // { id: 3, name: 'Al Integrated Online Community', value: 'Revolutionizing Lifestyle' },
   // { id: 4, name: 'Paid out to creators', value: '$70M' },
 ]
 
 export default function Statistics() {
   return (
     <div className="relative isolate overflow-hidden bg-gray-900 py-24 sm:py-32">
-      {/* <img
-        src="https://images.unsplash.com/photo-1522071820081-009f0129c71c?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2850&q=80&blend=111827&blend-mode=multiply&sat=-100&exp=15"
+      <img
+        src="pic/background_piano.png"
         alt=""
-        className="absolute inset-0 -z-10 h-full w-full object-cover"
-      /> */}
+        className="absolute inset-0 -z-20 h-full w-full object-cover"
+      />
+      <div className="absolute inset-0 -z-10 bg-gradient-to-t from-black via-black/60 to-black opacity-90"></div>
       <div className="relative mx-auto max-w-7xl px-6 lg:px-8">
         <div
           className="absolute -bottom-8 -left-96 -z-10 transform-gpu blur-3xl sm:-bottom-64 sm:-left-40 lg:-bottom-32 lg:left-8 xl:-left-10"
           aria-hidden="true"
         >
-          {/* <div
-            className="aspect-[1266/975] w-[79.125rem] bg-gradient-to-tr from-[#ff4694] to-[#776fff] opacity-20"
-            style={{
-              clipPath:
-                'polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)',
-            }}
-          /> */}
         </div>
         <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl">
           <h2 className="text-base font-semibold leading-8 text-indigo-400">Base our search</h2>
@@ -46,5 +39,5 @@ export default function Statistics() {
         </dl>
       </div>
     </div>
-  )
+  );
 }
